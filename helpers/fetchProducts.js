c84243ea03271435ec4ac9d61714b950226ffc64
Lais <laismassacesi@gmail.com>
2022-05-09 19:45:49 -0300

    Ajusta função fetchProducts para estourar um erro

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,4 +1,7 @@
 const fetchProducts = async (param) => {
+  if (!param) {
+    throw new Error('You must provide an url');
+  }
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
   const p = await fetch(endpoint);
   const r = await p.json();
